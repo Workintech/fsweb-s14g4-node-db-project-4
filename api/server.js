b0659652b2ce09be463c57fd96828d605a0216dc
samilkafa <samilkafa@gmail.com>
2023-08-03 03:17:59 +0300

    Hafta-14-Proje-4

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const server = express();
+const tarifRouter = require("./Tarifler/tarif-router");
 server.use(express.json());
 
 server.get("/", (req, res) => {
     res.json("App is working...")
-})
+});
+
+server.use("/api/Tarifler", tarifRouter);
 
 module.exports = server;
